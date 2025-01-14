cc8cf2fcee636ad61d601efb07b3ae0ebb018654
Francesco Carlucci <francesco.carlucci6@studio.unibo.it>
2025-01-14 18:47:50 +0100

    Code clean up

--- src/main.py
+++ src/main.py
@@ -18,22 +18,7 @@ def move_spectator_to(transform, spectator, distance=5.0, x=0, y=0, z=3, yaw=0, 
     transform.rotation.roll = roll
     
     spectator_transform = carla.Transform(back_location, transform.rotation)
-    
-    # spectator.set_transform(spectator_transform)
-
-def radar_callback(data: carla.RadarMeasurement):
-    global min_ttc, min_distance, absolute_speed
-    min_ttc = float('inf')
-
-    for detection, i in zip(data, range(len(data))):
-        absolute_speed = abs(detection.velocity)
-
-        # Calculate TTC
-        if absolute_speed != 0:
-            ttc = detection.depth / absolute_speed
-            if ttc < min_ttc:
-                min_ttc = ttc
-
+    spectator.set_transform(spectator_transform)
 
 def spawn_vehicle(vehicle_index=0, spawn_index=0, x_offset=0, y_offset=0, pattern='vehicle.*model3*', rotation=None) -> carla.Vehicle:
     blueprint_library = world.get_blueprint_library()
@@ -46,26 +31,21 @@ def spawn_vehicle(vehicle_index=0, spawn_index=0, x_offset=0, y_offset=0, patter
     vehicle = world.spawn_actor(vehicle_bp, spawn_point)
     return vehicle
 
-# Spawn ego vehicle
 # ego_vehicle = spawn_vehicle(x_offset=157, y_offset=-30)
 ego_vehicle = spawn_vehicle(x_offset=181, y_offset=-20, rotation=Rotation(yaw=180, pitch=0, roll=0))
 # ego_vehicle = spawn_vehicle(spawn_index=1, x_offset=10, y_offset=-9, rotation=Rotation(yaw=90, pitch=0, roll=0))
 
-radar_right = RadarSensor(ego_vehicle, "right", y=1, pitch=5, yaw=45)   # Attach right radar to 'vehicle'
-radar_left = RadarSensor(ego_vehicle, "left", y=-1, pitch=5, yaw=-45)  # Attach left radar to 'vehicle'
+radar_right = RadarSensor(ego_vehicle, "right", y=1, pitch=5, yaw=45)
+radar_left = RadarSensor(ego_vehicle, "left", y=-1, pitch=5, yaw=-45)
 
 radar_right.start_timer(3)
 radar_left.start_timer(3)
 
-target_vehicle_array = []
 # Spawn target vehicle for testing
-for i in range (2, 50): # 0 is the ego vehicle
+target_vehicle_array = []
+for i in range (1, 50): # 0 is the ego vehicle
     target_vehicle = spawn_vehicle(spawn_index=i)
     target_vehicle.set_autopilot()
-
-
-# Variable to store the minimum TTC
-min_ttc = float('inf')
 
 # time.sleep(4)
 # ego_vehicle.set_autopilot(True)
@@ -73,10 +53,8 @@ try:
     while True:
         # move_spectator_to(ego_vehicle.get_transform(), spectator)
         world.tick()
-
 except KeyboardInterrupt:
     print("Keyboard interrupt detected.")
-
 finally:
     vehicles = world.get_actors().filter('vehicle.*') 
     for vehicle in vehicles:
